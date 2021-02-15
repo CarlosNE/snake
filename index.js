36555c5f7f4396c171ee108732ea630b16f62101
Carlos Noel Eguibegui <carlos.eguibegui@sdc.com.ar>
2021-02-15 03:30:34 -0300

    first push

---
+++ index.js
@@ -0,0 +1,114 @@
+/*
+Introduccion: Juego de la viborita (Snake). Consiste en una vibora que va comiendo 'manzanas' (puntos rojos) que aparecen al azar por la pantalla.
+Pautas:
+1. Cada vez que la vibora come una manzana, se alarga de tamaño.
+2. Si la vibora colisiona con alguna parte de su cuerpo (por ejemplo su cola), esta vuelve al tamaño original.
+*/
+
+// Una vez que carga el html, se ejecuta la funcion cargarConfiguracion()
+window.onload = cargarConfiguracion()
+
+function cargarConfiguracion() {
+    contenedorPuntajeHtml = document.getElementById('puntaje')
+    contenedorPuntajeHtml.insertAdjacentHTML('beforeend', '<div id="puntajeActual">0</div>')
+    puntajeActual = document.getElementById('puntajeActual')
+    canvas = document.getElementById('canvas')
+    context = canvas.getContext('2d')
+    document.addEventListener("keydown", flechaPresionada)
+    objetivo = 100
+    // Por cada segundo, se llama a la funcion jugar() 15 veces en la cual se escucha que tecla presiona el usuario y la misma funcion se encarga de la logica del juego.
+    setInterval(() => {
+        if (puntos < objetivo)
+            game()
+        else
+            terminarJuego()
+    }, 1000 / 13);
+}
+
+// VARIABLES GLOBALES
+jugadorX = jugadorY = 10
+tabla = tileCount = 20
+manzanaX = manzanaY = 15
+velocidadX = velocidadY = 0
+cuerpoVibora = []
+colaVibora = 5
+puntos = 0
+function game() {
+    jugadorX += velocidadX
+    jugadorY += velocidadY
+    // Si el jugador se sale del canvas, aparece en el otro lado
+    if (jugadorX < 0) {
+        jugadorX = tileCount - 1
+    }
+    if (jugadorX > tileCount - 1) {
+        jugadorX = 0
+    }
+    if (jugadorY < 0) {
+        jugadorY = tileCount - 1
+    }
+    if (jugadorY > tileCount - 1) {
+        jugadorY = 0
+    }
+
+    // Se dibuja el fondo negro en el canvas
+    context.fillStyle = "black"
+    context.fillRect(0, 0, canvas.width, canvas.height)
+
+    // Se dibuja la vibora en el canvas
+    context.fillStyle = "lime"
+    for (var i = 0; i < cuerpoVibora.length; i++) {
+        context.fillRect(cuerpoVibora[i].x * tabla, cuerpoVibora[i].y * tabla, tabla - 2, tabla - 2)
+        // Si la vibora colisiona con su propio cuerpo, vuelve a su tamaño original
+        if (cuerpoVibora[i].x == jugadorX && cuerpoVibora[i].y == jugadorY) {
+            colaVibora = 5
+        }
+    }
+
+    // Guarda la ultima posicion de la vibora
+    cuerpoVibora.push({ x: jugadorX, y: jugadorY })
+    // Mientras la vibora ...
+    while (cuerpoVibora.length > colaVibora) {
+        cuerpoVibora.shift()
+    }
+    // Si la vibora colisiona con la manzana, se agranda y se crea una nueva manzana de forma aleatoria
+    if (manzanaX == jugadorX && manzanaY == jugadorY) {
+        puntos += 10
+        puntajeActual.innerHTML = puntos
+        colaVibora++
+        manzanaX = Math.floor(Math.random() * tileCount)
+        manzanaY = Math.floor(Math.random() * tileCount)
+    }
+
+    // Se dibuja la manzana en el canvas
+    context.fillStyle = "red"
+    context.fillRect(manzanaX * tabla, manzanaY * tabla, tabla - 2, tabla - 2)
+}
+
+// Funcion que termina la ejecucion del juego
+function terminarJuego() {
+    // Se dibuja el mensaje en el canvas
+    context.fillStyle = "white"
+    context.font = "30px Roboto";
+    context.fillText("FELICIDADES GANASTE!", 40, 200);
+}
+// Depende que flecha se hmanzanaYa presionado, la vibora se movera en una direccion u otra.
+function flechaPresionada(evento) {
+    switch (evento.keyCode) {
+        case 37:
+            velocidadX = -1
+            velocidadY = 0
+            break
+        case 38:
+            velocidadX = 0
+            velocidadY = -1
+            break
+        case 39:
+            velocidadX = 1
+            velocidadY = 0
+            break
+        case 40:
+            velocidadX = 0
+            velocidadY = 1
+            break
+    }
+}
